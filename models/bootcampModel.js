a02a16f0092b26be5c91b68f42b5d7a378baadd1
DTaitt <donovan.taitt@yahoo.com>
2017-06-13 16:23:55 -0400

    joined locations into bootcamps display

--- models/bootcampModel.js
+++ models/bootcampModel.js
@@ -6,10 +6,12 @@ Bootcamp.findAll = function() {
 	// return db.query('SELECT * FROM careers');
 	return db.query(
 		`
-			SELECT name, field, price, length, location_id 
+			SELECT name, field, price, length, city
 			FROM bootcamps 
 			LEFT JOIN careers 
-			ON bootcamps.career_id = careers.id;
+			ON bootcamps.career_id = careers.id
+			LEFT JOIN locations 
+			ON bootcamps.location_id = locations.id;
 		`
 	);
 };
