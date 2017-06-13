37965b684443ddebe0c420723fbaa72fcc040888
DTaitt <donovan.taitt@yahoo.com>
2017-06-13 13:11:42 -0400

    seeded bootcamp data

---
+++ db/seeds/bootcamps.sql
@@ -0,0 +1,273 @@
+\c bluprint_development
+
+INSERT INTO bootcamps (name, career_id, price, length, location_id) VALUES
+  (
+    'flatiron school',
+    1,
+    15000,
+    15,
+    1
+  ),
+  (
+    'fullstack academy',
+    1,
+    17610,
+    13,
+    1
+  ),
+  (
+    'fullstack academy',
+    1,
+    17610,
+    13,
+    7
+  ),
+  (
+    'general assembly',
+    1,
+    14950,
+    12,
+    1
+  ),
+  (
+    'general assembly',
+    1,
+    14950,
+    12,
+    2
+  ),
+  (
+    'general assembly',
+    1,
+    14950,
+    12,
+    3
+  ),
+  (
+    'general assembly',
+    1,
+    14950,
+    12,
+    4
+  ),
+  (
+    'general assembly',
+    1,
+    14950,
+    12,
+    6
+  ),
+  (
+    'general assembly',
+    1,
+    14950,
+    12,
+    7
+  ),
+  (
+    'startup institute',
+    1,
+    9500,
+    8,
+    1
+  ),
+  (
+    'startup institute',
+    1,
+    9500,
+    8,
+    4
+  ),
+  (
+    'coding dojo',
+    1,
+    12500,
+    14,
+    3
+  ),
+  (
+    'coding dojo',
+    1,
+    12500,
+    14,
+    6
+  ),
+  (
+    'coding dojo',
+    1,
+    12500,
+    14,
+    7
+  ),
+  (
+    'code fellows',
+    1,
+    12000,
+    10,
+    6
+  ),
+  (
+    'actualize',
+    1,
+    13900,
+    16,
+    3
+  ),
+  (
+    'general assembly',
+    2,
+    14950,
+    10,
+    1
+  ),
+  (
+    'general assembly',
+    2,
+    14950,
+    10,
+    2
+  ),
+  (
+    'general assembly',
+    2,
+    14950,
+    10,
+    3
+  ),
+  (
+    'general assembly',
+    2,
+    14950,
+    10,
+    4
+  ),
+  (
+    'general assembly',
+    2,
+    14950,
+    10,
+    6
+  ),
+  (
+    'general assembly',
+    2,
+    14950,
+    10,
+    7
+  ),
+  (
+    'startup institute',
+    2,
+    9500,
+    10,
+    1
+  ),
+  (
+    'startup institute',
+    2,
+    9500,
+    10,
+    4
+  ),
+  (
+    'general assembly',
+    5,
+    15950,
+    12,
+    1
+  ),
+  (
+    'general assembly',
+    5,
+    15950,
+    12,
+    2
+  ),
+  (
+    'general assembly',
+    5,
+    15950,
+    12,
+    3
+  ),
+  (
+    'general assembly',
+    5,
+    15950,
+    12,
+    4
+  ),
+  (
+    'general assembly',
+    5,
+    15950,
+    12,
+    6
+  ),
+  (
+    'general assembly',
+    5,
+    15950,
+    12,
+    7
+  ),
+  (
+    'galvanize',
+    5,
+    16000,
+    12,
+    2
+  )
+  ,
+  (
+    'galvanize',
+    5,
+    16000,
+    12,
+    6
+  ),
+  (
+    'metis',
+    5,
+    15500,
+    12,
+    1
+  )
+  ,
+  (
+    'metis',
+    5,
+    15500,
+    12,
+    2
+  ),
+  (
+    'metis',
+    5,
+    15500,
+    12,
+    6
+  )
+  ,
+  (
+    'metis',
+    5,
+    15500,
+    12,
+    7
+  ),
+  (
+    'level',
+    5,
+    7995,
+    20,
+    4
+  )
+  ,
+  (
+    'level',
+    5,
+    7995,
+    20,
+    6
+  );
